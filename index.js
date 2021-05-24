118d6c09dd355b48949d977c2f1d9524398ec1a3
erkaltufekci <erkaltufekci@gmail.com>
2021-05-24 20:27:59 +0200

    Add consumeCO2 method

--- index.js
+++ index.js
@@ -2,5 +2,25 @@ class User {
   constructor(name, email) {
     this.name = name;
     this.email = email;
+    this.limit = 100;
+  }
+  consumeCO2(action) {
+    switch (action) {
+      case "driving car":
+        this.limit = -50;
+        console.log(`This is your actual limit ${this.limit}`);
+        break;
+      case "ride a bike":
+        this.limit = -1;
+        console.log(`This is your actual limit ${this.limit}`);
+        break;
+      case "get a train":
+        this.limit = -10;
+        console.log(`This is your actual limit ${this.limit}`);
+        break;
+    }
   }
 }
+const ahmet = new User("ahmet", "ahmet@ahmet.de");
+ahmet.consumeCO2("driving car");
+console.log(ahmet);
