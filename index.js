33fb90beb5b7511b86e6d425e6a03f1e8603832c
erkaltufekci <erkaltufekci@gmail.com>
2021-05-27 00:20:24 +0200

    Recreate User Class

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
-class User {
-  constructor(name, email) {
-    this.name = name;
-    this.email = email;
-    this.limit = 100;
-    this.comment = [];
-  }
+const User = require("./user");
+const Mobility = require("./mobility");
 
-  consumeCO2(action) {
-    switch (action) {
-      case "driving car":
-        this.limit -= 50;
-        console.log(`This is your actual limit ${this.limit}`);
-        break;
-      case "ride a bike":
-        this.limit -= 1;
-        console.log(`This is your actual limit ${this.limit}`);
-        break;
-      case "get a train":
-        this.limit -= 10;
-        console.log(`This is your actual limit ${this.limit}`);
-        break;
-    }
-  }
-
-  commentAdd(person, text) {
-    this.comment.push(text);
-    console.log(`${person} made ${this.comment.length} times`);
-  }
-}
 const ahmet = new User("ahmet", "ahmet@ahmet.de");
-ahmet.consumeCO2("driving car");
-ahmet.commentAdd("mehmet", "hello");
-console.log(ahmet.comment);
+const serhat = new User("Serhat", "serhat@serhat.de");
+
+const car = new Mobility(10);
+
+serhat.footPrintCalc(10);
+
+console.log(car.carEmission);
+
+console.log(serhat.footPrintCalc());
