6f6b9ce8bc82231abce05b9e8f8857032b109162
erkaltufekci <erkaltufekci@gmail.com>
2021-05-28 22:20:56 +0200

    Transfer to src file

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const User = require("./user");
+const Mobility = require("./mobility");
+
+const ahmet = new User("ahmet", "ahmet@ahmet.de");
+const serhat = new User("Serhat", "serhat@serhat.de");
+
+const car = new Mobility(10);
+
+serhat.footPrintCalc(10);
+
+console.log(car.carEmission);
+
+console.log(serhat.footPrintCalc(car.carEmission));
