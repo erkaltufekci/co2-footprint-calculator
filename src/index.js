c650185ddb741ab42fcfc507b81d406311fc78e3
erkaltufekci <erkaltufekci@gmail.com>
2021-06-02 22:41:52 +0200

    Reorginize index file

--- src/index.js
+++ src/index.js
@@ -1,28 +1,19 @@
 const User = require('./user');
 
 const serhat = new User('Serhat', 'serhat@serhat.de');
+const ahmet = new User('Ahmet', 'ahmet@ahmet.de');
 
 serhat.driveCar(30);
 serhat.getOnTrain(10);
 serhat.rideBike(100);
 
+ahmet.getOnTrain(25);
+ahmet.rideBike(80);
+
 serhat.emission.storeTotalEmission(serhat.dailyTravel);
+ahmet.emission.storeTotalEmission(ahmet.dailyTravel);
 
-// console.log("------testing user class------");
-// console.log(serhat);
-// console.log(ahmet);
+console.log(serhat.emission.calculateTotalEmission());
 
-console.log(serhat.receiveEmissionResult());
-console.log('-----------------');
-console.log(serhat.calculatedEmission);
-console.log('-----------------');
-console.log(serhat);
-console.log('-----------------');
-
-// const car = new Mobility(10);
-
-// serhat.footPrintCalc(10);
-
-// console.log(car.carEmission);
-
-// console.log(serhat.footPrintCalc(car.carEmission));
+// console.log(serhat.emission.totalEmission);
+// console.log(ahmet.emission.totalEmission);
