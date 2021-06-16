630ce77fbcc05fc981dce4abcf24bcdf069705da
erkaltufekci <erkaltufekci@gmail.com>
2021-06-16 23:26:09 +0200

    Create Frontend

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const express = require('express');
 
 const router = express.Router();
@@ -6,19 +7,21 @@ const User = require('../models/user');
 require('../models/emission');
 
 /* GET users listing. */
-router.get('/', async (req, res) => {
+router.get('/init', async (req, res) => {
   const serhat = new User({ name: 'serhat', email: 'serhat@user.de' });
   const steve = new User({ name: 'steve', email: 'steve@user.de' });
   const john = new User({ name: 'john', email: 'john@user.de' });
+  console.log(serhat, 'hello');
   await serhat.save();
   await steve.save();
   await john.save();
 
-  res.send(400);
+  res.sendStatus(200);
 });
 
-router.get('/get', async (req, res) => {
-  const result = await User.find({ name: 'serhat' });
+router.get('/:userId', async (req, res) => {
+  const { userId } = req.params;
+  const result = await User.find({ _id: userId });
   res.send(result);
 });
 
