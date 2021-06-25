e45e3473df2a4903c70789cc68bd2ce4a22ae30c
erkaltufekci <erkaltufekci@gmail.com>
2021-06-25 23:14:08 +0200

    Add user-card component

---
+++ frontend/src/components/user-card.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: 'UserCard',
+  props: ['user'],
+  data() {
+    return {
+      // defaultBio: 'This user has no bios.',
+    };
+  },
+};
+</script>
+
+<template lang="pug">
+.box
+  h1 {{ user.name }} ({{ user.email }})
+</template>
+
+<style lang="scss" scoped>
+.box {
+  padding: 2rem;
+  border: 1px solid #333;
+  background: #dd0;
+  border-radius: 0.3rem;
+}
+</style>
