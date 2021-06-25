01f1e800ac60ba9a683b9d4c63ddef765566be51
erkaltufekci <erkaltufekci@gmail.com>
2021-06-25 23:12:14 +0200

    Update index.js

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,34 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
+const mutations = {
+  INCREMENT_COUNT: 'increment count',
+};
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    count: 0,
+  },
+  mutations: {
+    [mutations.INCREMENT_COUNT](state) {
+      state.count++;
+    },
+  },
+  actions: {
+    incrementCount({ commit }) {
+      commit(mutations.INCREMENT_COUNT);
+    },
+    async fetchUser(store, id) {
+      const userRequest = await axios.get(`/api/users/${id}`);
+      return userRequest.data;
+    },
+    async fetchUsers() {
+      const usersRequest = await axios.get('/api/users');
+      return usersRequest.data;
+    },
+  },
   modules: {},
 });
