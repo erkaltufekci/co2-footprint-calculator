27178350eaafdc61ae4c878776437c9398ac6c0e
erkaltufekci <erkaltufekci@gmail.com>
2021-06-25 23:26:27 +0200

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,19 @@
+/* eslint-disable no-unused-vars */
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const passport = require('passport');
+const User = require('./models/user');
+
+const mongooseConnection = require('./database-connection');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const accountRouter = require('./routes/account');
 
 const app = express();
 
@@ -17,10 +25,38 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(
+  session({
+    secret: ['thisisnotasupersecuresecretsecret', 'thisisanothersupernotsosecretsecret'],
+    store: MongoStore.create({ mongoUrl: process.env.MONGODB_CONNECTION_STRING, stringify: false }),
+    cookie: {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      path: '/api',
+    },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', (req, res, next) => {
+  req.session.viewCount = req.session.viewCount || 0;
+  // eslint-disable-next-line no-plusplus
+  req.session.viewCount++;
+  next();
+});
+
+app.use('/api/', indexRouter);
+app.use('/api/account', accountRouter);
+app.use('/api/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -36,9 +72,12 @@ app.use((err, req, res, next) => {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
-});
 
-require('./database-connection');
+  res.send({
+    status: err.status,
+    message: err.message,
+    stack: req.app.get('env') == 'development' ? err.stack : '',
+  });
+});
 
 module.exports = app;
