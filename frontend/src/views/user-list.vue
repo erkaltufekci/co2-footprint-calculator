ab66b32039a60cd3ae570eb0f18f4ae57444c8c5
erkaltufekci <erkaltufekci@gmail.com>
2021-06-25 23:03:58 +0200

    Add user-list.vue

---
+++ frontend/src/views/user-list.vue
@@ -0,0 +1,31 @@
+<script>
+import Counter from '@/components/counter.vue';
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'UserList',
+  components: { Counter },
+  data() {
+    return {
+      users: [],
+      time: new Date(),
+    };
+  },
+  async created() {
+    this.users = await this.fetchUsers();
+  },
+  methods: {
+    ...mapActions(['fetchUsers']),
+  },
+};
+</script>
+
+<template lang="pug">
+  .home
+    h1 CO2 Footprint Calculator
+    p The time is: {{ time }}
+    h2 Users
+    div(v-for="user in users")
+      router-link(:to="`/users/${user._id}`") {{ user.name }}
+    Counter
+</template>
