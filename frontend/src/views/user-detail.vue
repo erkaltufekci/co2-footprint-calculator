da1958d9fda24df0f28e620ea20002a8f845b38e
erkaltufekci <erkaltufekci@gmail.com>
2021-06-25 23:04:24 +0200

    Add user-detail.vue

---
+++ frontend/src/views/user-detail.vue
@@ -0,0 +1,28 @@
+<script>
+import Counter from '@/components/counter.vue';
+import UserCard from '@/components/user-card.vue';
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'UserDetail',
+  components: { UserCard, Counter },
+  data() {
+    return {
+      user: null,
+    };
+  },
+  async created() {
+    this.user = await this.fetchUser(this.$route.params.id);
+  },
+  methods: {
+    ...mapActions(['fetchUser']),
+  },
+};
+</script>
+
+<template lang="pug">
+  .about
+    h1 This is a user detail
+    UserCard(:user="user" v-if="user")
+    Counter
+</template>
