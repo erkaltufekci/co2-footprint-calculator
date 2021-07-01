a2d3a7cd3070e2191957c247d12836118df99273
Merve Begüm Tüfekçi <begum@Merve-MBP.local>
2021-07-01 11:49:19 +0200

    Implement web sockets

---
+++ frontend/src/views/profile.vue
@@ -0,0 +1,55 @@
+<script>
+import Counter from '@/components/counter.vue'
+import { mapActions, mapState } from 'vuex'
+
+export default {
+  name: 'Profile',
+  components: { Counter },
+  data() {
+    return {
+      users: [],
+      time: new Date(),
+      message: ''
+    }
+  },
+  async created() {
+    this.users = await this.fetchUsers()
+  },
+  methods: {
+    ...mapActions(['fetchUsers', 'goLive', 'sendMessageToLiveStream', 'joinStream']),
+    sendMessage(e) {
+      e.preventDefault()
+      this.sendMessageToLiveStream(this.message)
+      this.message = ''
+    }
+  },
+  computed: {
+    ...mapState(['currentLiveStream', 'liveStreams', 'user', 'liveStreamMessages'])
+  }
+}
+</script>
+
+<template lang="pug">
+  .home(v-if="user")
+    h1 CO2 Footprint Calculator {{ user.name }}
+    p The time is: {{ time }}
+    h2 Users
+    div(v-for="user in users")
+      router-link(:to="`/users/${user._id}`") {{ user.name }}
+    div(v-if="liveStreams.length")
+      h2 Live streams
+      div(v-for="stream in liveStreams")
+        p {{ stream }}
+        button(@click="joinStream(stream)") Join stream
+    button(@click="goLive") Go live
+    div(v-if="currentLiveStream")
+      h3 Live stream
+      .messages
+        .message(v-for="message in liveStreamMessages")
+          p
+            span {{ message.author }}:&nbsp;
+            span {{ message.body }}
+      form(@submit="sendMessage")
+        input(type="text" v-model="message")
+        input(type="submit" value="Send message")
+</template>
